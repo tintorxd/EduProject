7743a7ab46bcc002578c4ae1be6b96535ef219f1
alex florido <elnagas123@gmail.com>
2023-03-27 16:50:57 -0400

    porsiacaso

--- EduProject/app/Models/Cursos.php
+++ EduProject/app/Models/Cursos.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Cursos extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'titulo',
+        'descripcion',
+        'tipo_curso',
+        'max_personas',
+        'costo',
+        'duracion',
+        'unidad_duracion',
+    ];
 }
